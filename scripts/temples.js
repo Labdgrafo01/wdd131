1b083009837ddfcddc523f276f4955e2d6b5a02d
Haydee De la Sota Gracia <del23049@byui.edu>
2024-05-03 17:40:04 -0600

    week02

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,32 +1,15 @@
-// Espera a que el DOM esté completamente cargado
-document.addEventListener("DOMContentLoaded", function () {
-    // Obtén el elemento del primer párrafo del pie de página
-    var copyrightParagraph = document.querySelector("footer p:first-of-type");
-    // Obtén el elemento del segundo párrafo del pie de página
-    var lastModifiedParagraph = document.querySelector("footer p:nth-of-type(2)");
+document.addEventListener('DOMContentLoaded',function(){
+    //set footer currentyear
+    document.getElementById('currentyear').textContent=new Date().getFullYear();
+    
+    //set footer last modified date
+    document.getElementById("lastModified").textContent=document.lastModified;
 
-    // Obtiene el año actual
-    var currentYear = new Date().getFullYear();
-    // Establece el año de copyright en el primer párrafo del pie de página
-    copyrightParagraph.textContent = "© " + currentYear + " Haydee De la Sota Gracia";
-    // Cambia el color de texto del primer párrafo del pie de página
-    copyrightParagraph.style.color = "white";
-    // Utiliza la misma tipografía que en tu HTML
-    copyrightParagraph.style.fontFamily = window.getComputedStyle(document.body).fontFamily;
-
-    // Obtiene la fecha de la última modificación del documento
-    var lastModifiedDate = new Date(document.lastModified);
-    // Establece la fecha de la última modificación en el segundo párrafo del pie de página
-    lastModifiedParagraph.textContent = "Última modificación: " + lastModifiedDate;
-    // Cambia el color de texto del segundo párrafo del pie de página
-    lastModifiedParagraph.style.color = "yellow";
-    // Utiliza la misma tipografía que en tu HTML
-    lastModifiedParagraph.style.fontFamily = window.getComputedStyle(document.body).fontFamily;
-});
-
-
-
-const myInt = 5;
-const myFloat = 6.667;
-myInt;
-myFloat;
+    //add event listener to the menu button
+    const hamBotton=document.getElementById('menu')//select the menu button
+    const navigation=document.querySelector('.navigation');//select the navigation container
+    hamBotton.addEventListener('click',function(){
+        hamBotton.classList.toggle('open');
+        navigation.classList.toggle('open');
+    })
+})
